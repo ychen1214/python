2d3254bf32a16ddfdf569376e72f39d645493846
Yu <ychen1214@users.noreply.github.com>
2021-09-17 17:34:25 -0400

    module 3

--- Ex_Files_Adv_Python/Exercise Files/02 BuiltIn Functions/iterators_start.py
+++ Ex_Files_Adv_Python/Exercise Files/02 BuiltIn Functions/iterators_start.py
@@ -7,15 +7,25 @@ def main():
     daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
 
     # TODO: use iter to create an iterator over a collection
-
+    i = iter(days)
+    print(next(i))
+    print(next(i))
+    print(next(i))
     # TODO: iterate using a function and a sentinel
-
+    with open("D:\\python\\Ex_Files_Adv_Python\\Exercise Files\\02 BuiltIn Functions\\testfile.txt","r") as fp:
+        for line in iter(fp.readline,''):
+            print(line)
     # TODO: use regular interation over the days
-
+    for m in range(len(days)):
+        print(m, days[m])
     # TODO: using enumerate reduces code and provides a counter
-
+    for i,m in enumerate(days,start=1):
+        print(i,m)
     # TODO: use zip to combine sequences
-
-
+    for m in zip(days, daysFr):
+        print(m)
+    for i,m in enumerate(zip(days,daysFr),start=1):
+        print(i, m[0],"=",m[1]," in French") 
+     #zip function terminates on the shorter list combination   
 if __name__ == "__main__":
     main()
--- Ex_Files_Adv_Python/Exercise Files/02 BuiltIn Functions/utilfuncs_start.py
+++ Ex_Files_Adv_Python/Exercise Files/02 BuiltIn Functions/utilfuncs_start.py
@@ -6,13 +6,13 @@ def main():
     list1 = [1, 2, 3, 0, 5, 6]
     
     # TODO: any will return true if any of the sequence values are true
-    
+    print(any(list1))
     # TODO: all will return true only if all values are true
-    
+    print(all(list1))
     # TODO: min and max will return minimum and maximum values in a sequence
-    
+    print(min(list1))
+    print("max: ",max(list1))
     # TODO: Use sum() to sum up all of the values in a sequence
-    
+    print("sum: ", sum(list1))
 if __name__ == "__main__":
     main()
-    
