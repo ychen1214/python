f066ed713cac036c8c7c68d059ff857bf579ff12
Yu <ychen1214@users.noreply.github.com>
2021-09-18 10:11:09 -0400

    module 3 part 2

--- Ex_Files_Adv_Python/Exercise Files/02 BuiltIn Functions/transforms_start.py
+++ Ex_Files_Adv_Python/Exercise Files/02 BuiltIn Functions/transforms_start.py
@@ -2,19 +2,27 @@
 
 
 def filterFunc(x):
-    pass
+    if x % 2 == 0:
+        return False
+    return True
 
 
 def filterFunc2(x):
-    pass
+    if x.isupper():
+        return False
+    return True
 
 
 def squareFunc(x):
-    pass
+    return x**2
 
 
 def toGrade(x):
-    pass
+    if (x >= 90):
+        return "A"
+    elif (x >= 80 and x < 90):
+        return "B"
+    return "F"
 
 
 def main():
@@ -24,13 +32,19 @@ def main():
     grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
     # TODO: use filter to remove items from a list
-
+    odds = list(filter(filterFunc,nums))
+    print(odds)
     # TODO: use filter on non-numeric sequence
-
+    lowers = list(filter(filterFunc2,chars))
+    print(lowers)
     # TODO: use map to create a new sequence of values
+    squares = list(map(squareFunc,nums))
+    print(squares)    
 
     # TODO: use sorted and map to change numbers to grades
-
+    grades = sorted(grades)
+    letters = list(map(toGrade,grades))
+    print(letters)
 
 if __name__ == "__main__":
     main()
