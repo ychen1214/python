f84f78ae7fdacf7db4ebc5fff4d98f0da2c73b0b
Yu <ychen1214@users.noreply.github.com>
2021-10-07 08:01:10 -0400

    scraper beautiful soup

---
+++ beautifulsoup/example.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import re
+
+with open("doc.html") as fp:
+    soup = BeautifulSoup(fp, "html.parser")
+    #print(soup.head.title)
+    #print(soup.body.a.text)
+    #print(soup.body.p.b)
+    #print(soup.find("a"))
+    #print(soup.find_all("a",class_="element"))
+    #print(soup.find_all("a",class_="avatar"))
+    #print(soup.find("a", href=True)["href"])
+    #for tag in soup.find_all(re.compile("^b")):
+    #    print(tag)
+    #print(soup.get_text)
+    print(soup.get_text())    
