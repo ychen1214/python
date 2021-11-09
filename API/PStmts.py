485303da7e40cb257dbd32b9874576e89cf4621e
Yu <ychen1214@gmail.com>
2021-11-09 12:58:57 -0500

    stmts

---
+++ API/PStmts.py
@@ -0,0 +1,24 @@
+
+a = 1 + 2 + \
+    4 + 5 + \
+    7    
+
+a =( 1 + 2 + \
+    4 + 5 + \
+    7    )
+
+colors =['red',
+        'blue',
+        'green']
+
+a = 1; b = 2; c =3
+
+a, b, c = 5, 3.2, "Hello"
+print(a)
+print(b)
+print(c)
+
+x=y=z="same"
+print(x)
+print(y)
+print(z)
