4cdf94170ed04fca84ecf920f58a846bc6dda277
Yu <ychen1214@gmail.com>
2021-11-12 10:17:25 -0500

    latests changes

--- API/functions.py
+++ API/functions.py
@@ -1,15 +1,3 @@
-def greet(name):
-    """
-    This function greets to the person passed in as a parameter
-    """
-    print("Hello, " + name + ". Good Morning!")
-
-greet('Paul')
-
-#doc string 
-print(greet.__doc__)
-
-
 def absolute_value(num):
     """
     This function returns the absolute value of the entered number
@@ -21,3 +9,30 @@ def absolute_value(num):
 
 print(absolute_value(2))        
 print(absolute_value(-4))        
+
+def greet(name, msg="Good Morning!"):
+    """
+    This function greets to 
+    the person with the
+    provided message.
+
+    If the message is not provided,
+    it defaults to "Good Morning!"
+    """
+    print("Hello", name + ', ' + msg)
+
+
+greet("Kate")
+greet("Bruce","how do you do?")
+#doc string 
+print(greet.__doc__)
+
+
+def greet2(*names):
+    """
+    This function greets all the person in the names tuple
+    """
+    #names is a tuple with arguments
+    for name in names:
+        print("Hello", name)
+greet2("Monica","Luke","Steve","John")        
