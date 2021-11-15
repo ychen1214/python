ac32be60dfcdc0a72107bc5a1784e1761c91d00f
Yu <ychen1214@gmail.com>
2021-11-15 10:00:24 -0500

    ops and ifs

---
+++ API/operators.py
@@ -0,0 +1,38 @@
+#arithmetic operators
+x = 15
+y = 4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
+
+
+
+#logical operators
+a = True
+b = True
+print(a and b)
+print(a or b)
+print(not a)
+
+#membership operators
+c = 'hello word'
+d = {1:'a', 2:'b'}
+
+# Output: True
+print('H' in c)
+
+# Output: True
+print('hello' not in c)
+
+# Output: True
+print(1 in d)
+
+# Output: False
+print('a' in d)
