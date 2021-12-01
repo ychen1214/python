abb97b39d1389254eecc66dd2aeffbfbfa080ce4
Yu <ychen1214@gmail.com>
2021-12-01 13:41:31 -0500

    generator and iterators

---
+++ API/iterators.py
@@ -0,0 +1,55 @@
+# define a list
+my_list = [4, 7, 0, 3]
+
+# get an iterator using iter()
+my_iter = iter(my_list)
+
+# iterate through it using next()
+
+# Output: 4
+print(next(my_iter))
+
+# Output: 7
+print(next(my_iter))
+
+# next(obj) is same as obj.__next__()
+
+# Output: 0
+print(my_iter.__next__())
+
+# Output: 3
+print(my_iter.__next__())
+
+# This will raise error, no items left
+# next(my_iter)
+
+
+# define a list
+my_list = [4, 7, 0, 3]
+
+for element in my_list:
+    print(element)
+
+
+class PowTwo:
+    """Class to implement an iterator
+    of powers of two"""
+
+    def __init__(self, max=0):
+        self.max = max
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n <= self.max:
+            result = 2 ** self.n
+            self.n += 1
+            return result
+        else:
+            raise StopIteration
+
+
+for i in PowTwo(5):
+    print(i)
