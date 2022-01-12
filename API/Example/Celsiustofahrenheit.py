05eaab860b66dffe877a9eb1d5da7993728883e4
Yu <yu.chen@harpercollins.com>
2022-01-12 09:51:33 -0500

    numbers

---
+++ API/Example/Celsiustofahrenheit.py
@@ -0,0 +1,11 @@
+#fahrenheit = celsius * 1.8 + 32
+
+# Python Program to convert temperature in celsius to fahrenheit 
+
+# change this value for a different result
+
+celsius = 37.5
+
+# calculate fahrenheit 
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))
